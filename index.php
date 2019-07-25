5eafc4813691c87ba37a76e8633a4ec3f937fd97
jennifer <jennifer.malleret@outlook.fr>
2019-07-25 11:46:17 +0200

    exercice 9 php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 9</title>
+</head>
+<body>
+  <h1>Créer une variable et l'afficher en majuscule</h1>
+  <?php
+  $maj = 'majuscule';
+  echo strtoupper($maj);
+  ?>
+</body>
+</html>
